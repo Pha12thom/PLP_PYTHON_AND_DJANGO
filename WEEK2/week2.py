37560d2854012f34f7b09444a2f2bed2d6e0b50c
pha12thom <geofreymilugo@gmail.com>
2024-03-03 22:42:11 +0300

    week 2

---
+++ WEEK2/week2.py
@@ -0,0 +1,34 @@
+"""
+Create an empty list called my_list.
+Append the following elements to my_list: 10, 20, 30, 40.
+Insert the value 15 at the second position in the list.
+Extend my_list with another list: [50, 60, 70].
+Remove the last element from my_list.
+Sort my_list in ascending order.
+Find and print the index of the value 30 in my_list.
+"""
+
+# Create an empty list called my_list
+my_list = []
+
+# Append the following elements to my_list: 10, 20, 30, 40
+my_list.extend([10, 20, 30, 40])
+
+# Insert the value 15 at the second position in the list
+my_list.insert(1, 15)
+
+# Extend my_list with another list: [50, 60, 70]
+my_list.extend([50, 60, 70])
+
+# Remove the last element from my_list
+my_list.pop()
+
+# Sort my_list in ascending order
+my_list.sort()
+
+# Find and print the index of the value 30 in my_list
+index_30 = my_list.index(30)
+print("Index of 30 in my_list:", index_30)
+
+print("Final my_list:", my_list)
+
